a7072324a9324d9ec963fd5ceff91b79ee198532
mwawrzonkowski <wawko2@gmail.com>
2020-09-25 13:34:37 +0200

    Updates

---
+++ precache-manifest.f66064ab3d3829015bbf4c12443a1bc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f2051528a6768ecdffef5471243ad3",
+    "url": "/Restauratorium/index.html"
+  },
+  {
+    "revision": "ef22dae9e0a23afae2f3",
+    "url": "/Restauratorium/static/js/2.7ad5b099.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Restauratorium/static/js/2.7ad5b099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "579d71c0935a8456a926",
+    "url": "/Restauratorium/static/js/main.2cff01c6.chunk.js"
+  },
+  {
+    "revision": "6fe4b80bba2385b33216",
+    "url": "/Restauratorium/static/js/runtime-main.38d8d1d9.js"
+  },
+  {
+    "revision": "68cbbfcb7a3d112beb271c38c4afb10b",
+    "url": "/Restauratorium/static/media/background.68cbbfcb.png"
+  },
+  {
+    "revision": "8297cd8d2ffe4f0a6ce859fadf738e41",
+    "url": "/Restauratorium/static/media/search-solid.8297cd8d.svg"
+  }
+]);
